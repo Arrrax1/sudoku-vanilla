aba7c356f91f1587aae72ded015bfd3982ee5abb
Arrrax1 <bimobimo134@gmail.com>
2023-07-02 19:49:33 +0100

    init board

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 window.onload = (document.querySelector(".loading-screen").style.display="none")
+
+// Initiate WebPage
+let container = document.querySelector(".container");
+//Board
+for (let row = 0; row < 9; row++) {
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    for (let col = 0; col < 9; col++) {
+        let input = document.createElement("input");
+        input.classList.add("tile");
+        input.id = row + "-" + col;
+        input.type = "number"
+        rowDiv.appendChild(input)
+    }
+    container.appendChild(rowDiv);
+}
